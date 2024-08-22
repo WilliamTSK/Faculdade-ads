bec3cb795a5eee9ae4713869e40989bd0d01f3d2
WilliamTSK <williamsas4k1@gmail.com>
2024-08-22 08:57:20 -0300

    Add files via upload

---
+++ Linguagem C/mediadenotas.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <locale.h>
+
+int main() {
+    setlocale(LC_ALL "Portuguese");
+    float mdn,npf,np;
+    char nome[30];
+    printf("informe seu nome: ")
+    gets(nome)
+    
+    
+    }

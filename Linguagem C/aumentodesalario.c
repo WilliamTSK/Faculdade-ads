aebff6455e7bb2f609bd8400d9f568128b413098
WilliamTSK <williamsas4k1@gmail.com>
2024-08-22 08:42:45 -0300

    Add files via upload

--- Linguagem C/aumentodesalario.c
+++ Linguagem C/aumentodesalario.c
@@ -4,10 +4,13 @@
 int main () {
     setlocale(LC_ALL,"Portuguese");
     float salarioatual,a;
+    char nome[30];
+    printf("informe seu nome: ");
+    gets(nome);
     printf("seu salario: ");
     scanf("%f",&salarioatual);
     a=salarioatual+salarioatual*15.35/100;
-    printf("seu salário é de %.2f\n",a);
+    printf("%s,seu salário é de %.2f\n",nome,a);
     system("pause");
     return 0;
 }

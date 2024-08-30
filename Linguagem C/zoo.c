138e5a3ba6de003a547c0f51c6dd078ac5a3e826
WilliamTSK <williamsas4k1@gmail.com>
2024-08-30 08:17:55 -0300

    Add files via upload

--- Linguagem C/zoo.c
+++ Linguagem C/zoo.c
@@ -1,7 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <locale.h>
 
 int main () {
+    setlocale(LC_ALL,"portuguese");
 	float custo,quantidade_Com,qcp,cd,qcm,cem;
 	int quantidade_Ani;
 	char nome [30];
@@ -9,17 +11,17 @@ int main () {
 	gets(nome);
 	printf("quantidade de animais nesse zoológico ");
 	scanf("%d",&quantidade_Ani);
-	printf("custo estimado ");
+	printf("custo estimado por kilo de comida R$ ");
 	scanf("%f",&custo);
 	printf("quantidade de comida por kilo que este animal consome por dia ");
 	scanf("%f",&qcp);
 	
 	cd=quantidade_Ani*custo*qcp;
-	printf("custo estimado por dia: %.2f\n",cd);
+	printf("custo estimado por dia: R$ %.2f\n",cd);
 	qcm=qcp*30*quantidade_Ani;
 	printf("quantidade de comida consumida por mês: %.2f\n",qcm);
 	cem=qcm*custo;
-	printf("custo estimado por mês: %.2f\n",cem);
+	printf("custo estimado por mês: R$ %.2f\n",cem);
 	
 	
 	system("pause");

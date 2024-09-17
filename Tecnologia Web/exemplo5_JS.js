3917d503825041676bd85e8647ed0f8bd43a84c7
WilliamTSK <williamsas4k1@gmail.com>
2024-09-17 08:50:05 -0300

    Add files via upload

---
+++ Tecnologia Web/exemplo5_JS.js
@@ -0,0 +1,13 @@
+var largura = screen.width;
+var altura = screen.height;
+
+var larguraUtil=screen.availWidth;
+var alturaUtil=screen.availHeight;
+
+document.write("largura da tela" + largura + "pixel") ;
+document.write("<br>");
+document.write("altura da tela" + altura + "pixel");
+document.write("<br><br>");
+document.write("largura da área utilizavel: "+ larguraUtil+"pixels");
+document.write("<br>");
+document.write("altura da área utilizavel: "+ alturaUtil+"pixels");

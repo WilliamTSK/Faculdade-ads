de15a0b7c880d28d0fb61727e594bcd970cde88b
WilliamTSK <williamsas4k1@gmail.com>
2024-10-18 08:52:25 -0300

    Add files via upload

---
+++ Python/python1.py
@@ -0,0 +1,19 @@
+nome="carlos"
+print(nome)
+sobrenome="oliveira"
+print(sobrenome)
+numero=12
+print(7+60)
+print(67-numero)
+salario=13456.32
+ok = True
+numero = 784
+numero = "aqui é um texto"
+print(type(numero))
+print(type(salario))
+print(type(ok))
+
+lista = [34,2,-3,0,45]
+print(type(lista))
+endereço = "Logo ali"
+print(endereço)

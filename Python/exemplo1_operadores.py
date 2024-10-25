60a9e57047681cf499a711da0f465be1f01f340a
WilliamTSK <williamsas4k1@gmail.com>
2024-10-25 08:57:14 -0300

    Add files via upload

---
+++ Python/exemplo1_operadores.py
@@ -0,0 +1,25 @@
+n1=20
+n2=3
+
+res=n1+n2
+print(f"{n1}+{n2}={res}")
+
+res=n1-n2
+print(f"{n1}-{n2}={res}")
+
+res=n1*n2
+print(f"{n1}*{n2}={res}")
+
+res=n1/n2
+print(f"{n1}/{n2}={res}")
+#o trecho ':.4f é para fornatar com 4 casa decimais
+print(f"{n1}/{n2}={res:.4f}")
+
+res=n1//n2
+print(f"{n1}//{n2}={res}")
+
+res=n1%n2
+print(f"{n1}%{n2}={res}")
+
+res=n1**n2
+print(f"{n1}**{n2}={res}")
---
+++ Python/imc.py
@@ -0,0 +1,4 @@
+m=float(input("Qual a massa?" ))
+a=float(input("Qual a altura? "))
+imc= m/(a*a)
+print(f"Seu imc é de: {imc:.2f}")
